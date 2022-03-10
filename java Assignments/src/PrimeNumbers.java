d7a8b47e5dbe96bda9078d58d5055e4ef89f3c66
Ayush Tamrakar <Ayush Tamrakar@LAPTOP-5C1UEH49>
2022-03-10 10:18:41 +0530

    This is the program of prime numbers in java

---
+++ java Assignments/src/PrimeNumbers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class PrimeNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the number");
+		int number = sc.nextInt();
+		
+		for (int i=1; i<=number ; i++) {
+			if() {
+				System.out.println(i+"prime");
+			}else {
+				System.out.println(i+"not prime");
+			}
+		}
+	}
+
+}
