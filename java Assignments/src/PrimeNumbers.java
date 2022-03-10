583d7dd439f506682969c3f4265b74d09ccbed57
ayushSebone <atamrakar@sebonetechnologies.com>
2022-03-10 23:27:45 +0530

    this is updated

--- java Assignments/src/PrimeNumbers.java
+++ java Assignments/src/PrimeNumbers.java
@@ -2,17 +2,26 @@ import java.util.*;
 public class PrimeNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
+
+		Scanner sc = new Scanner(System.in);	
 		System.out.println("Enter the number");
 		int number = sc.nextInt();
 		
-		for (int i=1; i<=number ; i++) {
-			if() {
-				System.out.println(i+"prime");
+		primes(number);	
+	}
+	public static void primes(int num) {
+		int temp = 0;
+		for(int i=1; i<num ; i++) {
+			for(int j = 2 ; j<= i-1;j++) {
+				if(i%j==0) {
+					temp = temp+1;
+				}
+				
+			}
+			if(temp ==0) {
+				System.out.println(i);
 			}else {
-				System.out.println(i+"not prime");
+				temp=0;
 			}
 		}
 	}
