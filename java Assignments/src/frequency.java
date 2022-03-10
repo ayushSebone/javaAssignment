5b035b0d0e8a00d9458a89b88d69117873327736
ayushSebone <atamrakar@sebonetechnologies.com>
2022-03-10 22:59:32 +0530

    updated

--- java Assignments/src/frequency.java
+++ java Assignments/src/frequency.java
@@ -1,36 +1,41 @@
 import java.util.*;
-public class Freq {
+public class frequency {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		Scanner sc = new Scanner(System.in);
-		
-		int count = 0;
-		
 		System.out.println("Enter the article");
 		String scr1 = sc.nextLine();
 		System.out.println("Enter the word");
 		String scr2 = sc.nextLine();
 		
+		wordIn(scr1,scr2);
+		
+	}
+	public static void wordIn(String s1, String s2) {
+		
+		int count = 0;
 		String temp = "";
 		
-		for(int i=0; i<scr1.length(); i++) {
+		for(int i=0; i<s1.length(); i++) {
 			 
-			if(scr1.charAt(i) != ' ') {
-				temp = temp + scr1.charAt(i);
+			if(s1.charAt(i) != ' ') {
+				temp = temp + s1.charAt(i);
 				
 			}else {
-				if (scr2.equals(temp))
+				if (s2.equals(temp))
 				{
 					count ++;
 				}
 				temp = "";
 			}
 		}
-		if(scr2.equals(temp)) {
+		if(s2.equals(temp)) {
 			count ++;
 		}
 		System.out.println(count);
+		return;
+		
 	}
 
 }
