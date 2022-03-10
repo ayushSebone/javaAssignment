e7df5c4eb3ee84051ec41475aaf6d752cf77a27c
ayushSebone <atamrakar@sebonetechnologies.com>
2022-03-10 22:17:31 +0530

    updated this file

---
+++ java Assignments/src/permutation.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class permutation {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the String");
+		String str = sc.nextLine();
+		printPermutations(str ,"");
+		
+	}
+	public static void printPermutations(String strq , String ans) {
+		if(strq.length() == 0) {
+			System.out.println(ans);
+			return;
+		}
+		
+		for(int i =0; i<strq.length();i++) {
+			char ch = strq.charAt(i);
+			String lpart = strq.substring(0, i);
+			String rpart = strq.substring(i+1);
+			String restOfQ = lpart + rpart;
+			printPermutations(restOfQ , ans + ch );
+		}
+		
+		
+	}
+
+}
